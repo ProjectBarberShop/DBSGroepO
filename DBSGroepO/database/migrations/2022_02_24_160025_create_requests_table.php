3d7ea54c9374611d24c2b7ba34066e61cac5aa02
JeroenE2000 <jmh.elst@student.avans.nl>
2022-02-24 17:56:22 +0100

    database migrations

---
+++ DBSGroepO/database/migrations/2022_02_24_160025_create_requests_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact-requests', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->require;
+            $table->string('firstname')->require;
+            $table->string('preprosition');
+            $table->string('lastname')->require;
+            $table->string('email');
+            $table->string('phonenumber');
+            $table->mediumtext('message')->require;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact-requests');
+    }
+};
