8c7fa9743d659c34fe6a8d79cec152fb21418d62
Barthol1 <bt.hol@student.avans.nl>
2022-03-15 11:47:13 +0100

    Contactseeder fix

--- DBSGroepO/database/seeders/ContactSeeder.php
+++ DBSGroepO/database/seeders/ContactSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Carbon;
 
 class ContactSeeder extends Seeder
 {
@@ -20,7 +23,7 @@ class ContactSeeder extends Seeder
             'preprosition' => str::random(10),
             'lastname' => str::random(10),
             'email' =>  Str::random(10).'@gmail.com',
-            'phonenumber' => int::random(10), 
+            'phonenumber' => rand(1,10), 
             'message' => str::random(256),
         ]);
     }
