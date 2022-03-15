24713669933a616445c549955b2ddae5b9443e47
ShootMeBigTime <70698888+ShootMeBigTime@users.noreply.github.com>
2022-03-15 11:02:19 +0100

    Optredens pagina

--- DBSGroepO/resources/views/preformance/preformance.blade.php
+++ DBSGroepO/resources/views/preformance/preformance.blade.php
@@ -3,13 +3,14 @@
 @section('content')
 <section id="contentpreformance">
     @foreach ($optredens as $pref)
-    <div class="d-block p-2">
-        <h3>{{ $pref->title}}: {{ date('d/M/Y',strtotime($pref->start))}}</h3>
-        <p>{{ $pref->description}}</p>
+    <div class="card">
+        <div class="d-block p-2">
+            <h3>{{ $pref->title}}: {{ date('d/M/Y',strtotime($pref->start))}}</h3>
+            <p>{{ $pref->description}}</p>
 
-        <label>Begint op: {{ date("H:i:s",strtotime($pref->start))}}</label><br>
-        <label>eindigt op: {{ date("H:i:s",strtotime($pref->end))}}</label>
-        <hr>
+            <label>Begint op: {{ date("H:i:s",strtotime($pref->start))}}</label><br>
+            <label>eindigt op: {{ date("H:i:s",strtotime($pref->end))}}</label>
+        </div>
     </div>
     
     @endforeach
