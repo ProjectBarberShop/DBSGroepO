3c6964f95fd38810146e2c61c41efffadad02428
ShootMeBigTime <70698888+ShootMeBigTime@users.noreply.github.com>
2022-04-07 18:25:57 +0200

    Update PerformanceController.php

--- DBSGroepO/app/Http/Controllers/PerformanceController.php
+++ DBSGroepO/app/Http/Controllers/PerformanceController.php
@@ -9,9 +9,9 @@ class PerformanceController extends Controller
 {
     public function index()
     {
-        $preformances = Agendapunt::join('agendapunt_category', 'agenda.id', '=', 'agendapunt_category.agendapunt_id')
+        $performances = Agendapunt::join('agendapunt_category', 'agenda.id', '=', 'agendapunt_category.agendapunt_id')
         ->join('category', 'category.id', '=', 'agendapunt_category.category_id')
         ->where('agendapunt_category.category_id', '=', 1);
-        return view('performance.performance', ['optredens'=>$preformances->get()]);
+        return view('performance.performance', ['optredens'=>$performances->get()]);
     }
 }
