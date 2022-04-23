fb6aef984a99323292e86a66ba757fe11b336119
ShootMeBigTime <70698888+ShootMeBigTime@users.noreply.github.com>
2022-04-23 19:54:11 +0200

    Testen toegevoegd

---
+++ DBSGroepO/tests/Browser/ImagesSystemTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Browser;
+
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class ImagesSystemTest extends DuskTestCase
+{
+    public function testFotoToevoegen()
+    {
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/cms/home');
+            $browser->clickLink('More info');
+            $browser->visit('/cms/fotos');
+            $browser->attach('absolutePathToFile', 'photo');
+            $browser->type('title', 'Duif');
+            $browser->type('category', 'Dier');
+            $browser->press('+');
+            $browser->assertPathIs('/cms/fotos');
+        });
+
+    }
+
+    public function testFotoZoekenMetFilter()
+    {
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/cms/fotos');
+            $browser->select('Dier', 'filter');
+            $browser->press('Zoeken');
+            $browser->assertPathIs('/cms/fotos');
+	    $browser-assertSee('Duif');
+	    $browser-assertDontSee('Koor');
+        });
+
+    }
+    public function testFotoZoekenZonderFilter()
+    {
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/cms/fotos');
+            $browser->type('search', 'Duif');
+            $browser->press('Zoeken');
+            $browser->assertPathIs('/cms/fotos');
+	    $browser-assertSee('Duif');
+        });
+
+    }
+    public function testFotoVerwijderen()
+    {
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/cms/fotos');
+            $browser->press('');
+            $browser->assertPathIs('/cms/fotos');
+	    $browser-assertDontSee('Duif');
+        });
+
+    }
+}
