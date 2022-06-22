a195021d6ebad1fb8287b02d1fea5072b79017bd
ShootMeBigTime <70698888+ShootMeBigTime@users.noreply.github.com>
2022-06-22 16:39:20 +0200

    sidebar bij inlog fix

--- DBSGroepO/resources/views/layouts/app.blade.php
+++ DBSGroepO/resources/views/layouts/app.blade.php
@@ -115,7 +115,8 @@
                 @yield('content')
         @endif
         </div>
-        <div id="sidebar" class="my-5 p-3 h-100 mx-auto position-sticky sticky-top w-25 d-none d-sm-block">
+        <div id="sidebar">
+        <div class="my-5 p-3 h-100 mx-auto position-sticky sticky-top w-25 d-none d-sm-block">
             <section class="flex-shrink-1 bg-danger card h-100 mx-auto d-none d-sm-block">
                     <div class="h-75 card-body">
                         <div id="sidebarInfoLogin">
@@ -165,6 +166,7 @@
                         </div>
                     </div>
                 </section>
+            </div>
             </div>
     </main>
     {{--start footer--}}
